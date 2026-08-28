282e2e4aa6ac87e009a4b7f80a220cdc9272f643
safwanulkarimminhaj84-lgtm <safwanulkarimminhaj84@gmail.com>
2026-08-27 20:07:16 -0700

    Create 0011-container-with-most-water.js

---
+++ LeetCode/Two-Pointers/0011-container-with-most-water.js
@@ -0,0 +1,23 @@
+function maxArea(height,min){
+    let left = 0;
+    let right = height.length-1;
+    let max=0;
+
+    while(left<right){
+        let area = (right-left)*Math.min(height[left],height[right]);
+        max = Math.max(max,area);
+        if(height[left]<height[right]){
+            left++
+        }else{
+            right--;
+        }
+    }
+    return max;
+}
+
+
+
+
+
+
+
